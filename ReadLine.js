d34d1f3bde7cbc9c992ae575c613623090adf3d5
ZePhyRus-Codes <you@example.com>
2024-03-27 19:15:17 +0500

    first commit

---
+++ ReadLine.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+     input: process.stdin,
+     output: process.stdout,
+});
+
+rl.question("What is your name? ",function(answer){
+     console.log(`Oh, so your name is ${answer}`);
+     rl.close;
+});
+
+
+
+
+
+
+
---
+++ ReadPrompt.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+const input = prompt("What is your name? ");
+
+console.log(`Ohm so your name is ${input}`);
